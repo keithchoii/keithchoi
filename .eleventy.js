958b3cfe87cf788b2af0a14e58e15519d175a2ec
keith <12971633+keithchoii@users.noreply.github.com>
2023-11-26 17:51:00 -0800

    Starter content
    Adding content structure through html using 11ty's layouts and partials

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
 module.exports = config => {
+    config.addPassthroughCopy('./src/images/');
+
+    config.addCollection('work', collection => {
+        return collection
+            .getFilteredByGlob('./src/work/*.md')
+            .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+    });
+    config.addCollection('featuredWork', collection => {
+        return collection
+            .getFilteredByGlob('./src/work/*.md')
+            .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1))
+            .filter(x => x.data.featured);
+    });
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
         dir: {
-            input: 'src',
-            output: '_site'
+            input: 'src'
         }
     };
 };
--- src/index.md
+++ src/index.md
@@ -1,6 +1,11 @@
 ---
-title: 'Keith Choi'
+title: 'Keith Choi - Portfolio'
 layout: 'layouts/home.html'
+intro:
+  eyebrow: 'Keith Choi'
+  summary: 'Hello! My name is Keith and I am an aspiring Software Engineer with side interests in Machine Learning and Game Development'
+  buttonText: 'See my work'
+  buttonUrl: '/projects'
 ---
 
-buh boom bo
+ok
